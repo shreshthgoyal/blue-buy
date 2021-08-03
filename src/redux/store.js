addfe4d4dc2f602f60ca73e46ac5d68eb04bff45
Shreshth Goyal <73751013+shreshthgoyal@users.noreply.github.com>
2021-08-03 17:06:37 +0530

    Commiting updates

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,7 +6,7 @@ import rootReducer from './root-reducer';
 
 const middlewares = [];
 
-if(process.env.NODE_ENV !== 'development')
+if(process.env.NODE_ENV === 'development')
 {middlewares.push(logger);}
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
